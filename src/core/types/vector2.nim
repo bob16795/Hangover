12742c8602acc3931efbeec2f5636c9392291b25
bob16795 <prestosilver.ptp@gmail.com>
2022-04-18 13:04:40 -0400

    add vector class

---
+++ src/core/types/vector2.nim
@@ -0,0 +1,83 @@
+import math
+
+type
+  Vector2* = object
+    x*: float32
+    y*: float32
+
+# const
+
+proc newVector2*(x, y: float32): Vector2 =
+  result.x = x
+  result.y = y
+
+proc newVector2*(x, y: int): Vector2 =
+  result.x = x.float32
+  result.y = y.float32
+
+# operators
+
+proc `*`*(p: Vector2, i: float32): Vector2 =
+  result = p
+  result.x *= i
+  result.y *= i
+
+proc `*`*(i: float32, p: Vector2): Vector2 =
+  result = p
+  result.x *= i
+  result.y *= i
+
+proc `+`*(p: Vector2, a: Vector2): Vector2 =
+  result = p
+  result.x += a.x
+  result.y += a.y
+
+proc `-`*(p: Vector2, a: Vector2): Vector2 =
+  result = p
+  result.x -= a.x
+  result.y -= a.y
+
+proc `/`*(p: Vector2, i: float32): Vector2 =
+  result = p
+  result.x = (result.x / i).float32
+  result.y = (result.y / i).float32
+
+# equals operators
+
+proc `*=`*(p: var Vector2, i: float32) =
+  p.x *= i
+  p.y *= i
+
+proc `/=`*(p: var Vector2, i: float32) =
+  p.x = (p.x / i).float32
+  p.y = (p.y / i).float32
+
+proc `+=`*(p: var Vector2, a: Vector2) =
+  p.x += a.x
+  p.y += a.y
+
+proc `-=`*(p: var Vector2, a: Vector2) =
+  p.x -= a.x
+  p.y -= a.y
+
+# utils
+
+proc distance*(a, b: Vector2): float =
+  var cx, cy: float32
+  cx = (a.x - b.x).float32
+  cy = (a.y - b.y).float32
+  return sqrt(cx * cx + cy * cy)
+
+proc `angle=`*(p: var Vector2, radians: float32) =
+  p.x = cos(radians).float32
+  p.y = sin(radians).float32
+
+proc angle*(p: Vector2): float32 =
+  return arctan2(p.x.float32, p.y.float32)
+
+proc rotated*(p: Vector2, phi: float32): Vector2 =
+  result.angle = phi + p.angle
+  result = result * p.distance(newVector2(0, 0)).float32
+
+proc rotate*(p: var Vector2, phi: float32) =
+  p = p.rotated(phi)
---
+++ tests/textVector2.nim
@@ -0,0 +1,57 @@
+import unittest
+
+import gin2
+
+import math
+
+suite "Vector2":
+  test "vec class can create from 2":
+    var vec = newVector2(0.cint, 1.cint)
+    assert vec.x == 0
+    assert vec.y == 1
+
+  test "multiply":
+    var vec = newVector2(0, 1) * 2
+    assert vec.x == 0
+    assert vec.y == 2
+    vec = 2 * newVector2(0, 1)
+    vec *= 2
+    assert vec.x == 0
+    assert vec.y == 4
+
+  test "divide":
+    var vec = newVector2(0, 4) / 2
+    assert vec.x == 0
+    assert vec.y == 2
+    vec /= 2
+    assert vec.x == 0
+    assert vec.y == 1
+
+  test "add":
+    var vec = newVector2(0, 1) + newVector2(0, 1)
+    assert vec.x == 0
+    assert vec.y == 2
+    vec += newVector2(0, 1)
+    assert vec.x == 0
+    assert vec.y == 3
+
+  test "subtract":
+    var vec = newVector2(0, 1) - newVector2(0, 1)
+    assert vec.x == 0
+    assert vec.y == 0
+    vec -= newVector2(0, 1)
+    assert vec.x == 0
+    assert vec.y == -1
+
+  test "distance":
+    var a = newVector2(0, 0)
+    var b = newVector2(0, 3)
+
+    assert distance(a, b) == 3
+
+  test "angle":
+    var vec = newVector2(0, 1)
+    assert vec.angle == 0
+    vec.angle = PI
+    assert vec.x == -1
+    assert vec.y == 0
