372d94ff8af731fba4167a81134c5523cfb4f760
bob16795 <prestosilver.ptp@gmail.com>
2022-04-18 13:04:53 -0400

    convert rect to float32

--- src/core/types/rect.nim
+++ src/core/types/rect.nim
@@ -1,45 +1,45 @@
-import point
+import vector2
 
 type
   Rect* = object of RootObj
-    x*: cint
-    y*: cint
-    width*: cint
-    height*: cint
+    x*: float32
+    y*: float32
+    width*: float32
+    height*: float32
 
-proc newRect*(x, y: cint, width, height: cint): Rect =
+proc newRect*(x, y: float32, width, height: float32): Rect =
   result.x = x
   result.y = y
   result.width = width
   result.height = height
 
-proc newRect*(position, size: Point): Rect =
+proc newRect*(position, size: Vector2): Rect =
   result.x = position.x
   result.y = position.y
   result.width = size.x
   result.height = size.y
 
-proc size*(r: Rect): Point =
+proc size*(r: Rect): Vector2 =
   result.x = r.width
   result.y = r.height
 
-proc `size=`*(r: var Rect, size: Point) =
+proc `size=`*(r: var Rect, size: Vector2) =
   r.width = size.x
   r.height = size.y
 
-proc location*(r: Rect): Point =
+proc location*(r: Rect): Vector2 =
   result.x = r.x
   result.y = r.y
 
-proc `location=`*(r: var Rect, p: Point) =
+proc `location=`*(r: var Rect, p: Vector2) =
   r.x = p.x
   r.y = p.y
 
-proc offset*(r: Rect, offset: Point): Rect =
+proc offset*(r: Rect, offset: Vector2): Rect =
   result.x = r.x + offset.x
   result.y = r.y + offset.y
   result.width = r.width
   result.height = r.height
 
-proc center*(r: Rect): Point =
-  return newPoint(r.x + (r.width / 2).cint, r.y + (r.height / 2).cint)
+proc center*(r: Rect): Vector2 =
+  return newVector2(r.x + (r.width / 2).float32, r.y + (r.height / 2).float32)
