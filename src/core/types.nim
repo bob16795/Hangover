da5bed9599c8c168ac3eebb00dc6c21411bd04b4
bob16795 <prestosilver.ptp@gmail.com>
2022-04-18 13:08:39 -0400

    add include vector in types

--- src/core/types.nim
+++ src/core/types.nim
@@ -1,11 +1,13 @@
 import types/graphicsContext
 import types/texture
+import types/vector2
 import types/point
 import types/color
 import types/rect
 
 export graphicsContext
 export texture
+export vector2
 export point
 export color
 export rect
