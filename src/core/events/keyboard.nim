c9f7f524e1f279380bef937ac990e19b640d2366
bob16795 <prestosilver.ptp@gmail.com>
2022-05-02 15:10:34 -0400

    char keyboard events

--- src/core/events/keyboard.nim
+++ src/core/events/keyboard.nim
@@ -1,8 +1,18 @@
+import unicode
+
 createEvent(EVENT_PRESS_KEY)
 createEvent(EVENT_RELEASE_KEY)
+createEvent(EVENT_LINE_ENTER)
+createEvent(EVENT_START_LINE_ENTER)
+createEvent(EVENT_STOP_LINE_ENTER)
+
+var
+  lineInput = false
+  lineText = ""
 
 proc keyCb*(win: Window, key: Key, scanCode: int32, action: KeyAction,
     mods: set[ModifierKey]) =
+  if lineInput: return
   case action:
   of kaDown:
     var k = key
@@ -12,3 +22,9 @@ proc keyCb*(win: Window, key: Key, scanCode: int32, action: KeyAction,
     sendEvent(EVENT_RELEASE_KEY, addr k)
   else:
     discard
+
+proc charCb*(win: Window, r: Rune) =
+  if not lineInput: return
+  lineText &= $r
+  sendEvent(EVENT_LINE_ENTER, addr lineText)
+
